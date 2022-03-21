f9b300b82fd29212b27cec3663699951309e47ad
uenot <tobyueno@gmail.com>
2022-03-21 00:37:44 -0400

    colors and flickering

--- load_routes.py
+++ load_routes.py
@@ -3,16 +3,19 @@ import json
 import os
 import config
 
-response = requests.get(f'https://api-v3.mbta.com/routes?api_key={config.key}&filter[type]=0,1')
-data = response.json()['data']
-formatted = {}
-for route in data:
-    keys = ['direction_destinations', 'color', 'long_name']
-    print(route)
-    formatted[route['id']] = {k: route['attributes'][k] for k in keys}
-if not os.path.exists('data'):
-    os.makedirs('data')
-with open('data/routes.json', 'w') as f:
-    json.dump(formatted, f, indent=2)
+
+def load_routes():
+    response = requests.get(f'https://api-v3.mbta.com/routes?api_key={config.key}&filter[type]=0,1')
+    data = response.json()['data']
+    formatted = {}
+    for route in data:
+        keys = ['direction_destinations', 'color', 'long_name']
+        formatted[route['id']] = {k: route['attributes'][k] for k in keys}
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    with open('data/routes.json', 'w') as f:
+        json.dump(formatted, f, indent=2)
 
 
+if __name__ == "__main__":
+    load_routes()
--- map.py
+++ map.py
@@ -1,23 +1,51 @@
 import json
 import re
 
-with open('map.txt') as f:
-    mbta_map = f.read()
 
-with open('data/vehicles.json') as f:
-    vehicles = json.load(f)
+def create_map():
+    with open('map.txt') as f:
+        mbta_map = f.read()
 
-with open('data/stops.json') as f:
-    stops = json.load(f)
+    with open('data/vehicles.json') as f:
+        vehicles = json.load(f)
 
-for vehicle, vehicle_info in vehicles.items():
-    stop = vehicle_info['stop']
-    if stop is not None:
-        mbta_map = re.sub(f'( |\||[0-9])*{stop}( |\||[0-9])*', f' {vehicle_info["route"][0].upper()} ', mbta_map)
-for stop, stop_info in stops.items():
-    mbta_map = re.sub(f'( |\||[0-9])*{stop}( |\||[0-9])*',
-                      f' {stop_info["description"].split(" - ")[1][0].lower()} ',
-                      mbta_map)
-mbta_map = re.sub('( )([A-Za-z])( / )([A-Za-z])( )', r"\2/\4", mbta_map)
+    with open('data/stops.json') as f:
+        stops = json.load(f)
 
-print(mbta_map)
+    for vehicle, vehicle_info in vehicles.items():
+        stop = vehicle_info['stop']
+        if stop is not None:
+            replace_str = f'{vehicle_info["route"][0].upper()} '
+            if vehicle_info['current_status'] in ['IN_TRANSIT_TO', 'INCOMING_AT']:
+                replace_str = 'f' + replace_str
+            replace_str = ' ' + replace_str
+            mbta_map = re.sub(f'( |\||[0-9])*{stop}( |\||[0-9])*', replace_str, mbta_map)
+    for stop, stop_info in stops.items():
+        mbta_map = re.sub(f'( |\||[0-9])*{stop}( |\||[0-9])*',
+                          f' {stop_info["description"].split(" - ")[1][0].lower()} ',
+                          mbta_map)
+    mbta_map = re.sub('( )([A-Za-z]*)( / )([A-Za-z]*)( )', r"\2/\4", mbta_map)
+
+    return mbta_map
+
+def match_lower(match):
+    return match.group(1).lower()
+
+def match_upper(match):
+    return match.group(1).upper()
+
+def flicker_map(mbta_map, state):
+    if state:
+        return re.sub('f([A-Z])', match_upper, mbta_map)
+    else:
+        return re.sub('f([A-Z])', match_lower, mbta_map)
+
+if __name__ == "__main__":
+    mbta_map = create_map()
+    print(mbta_map)
+    print()
+    flicker_1 = flicker_map(mbta_map, True)
+    print(flicker_1)
+    print()
+    flicker_2 = flicker_map(mbta_map, False)
+    print(flicker_2)
