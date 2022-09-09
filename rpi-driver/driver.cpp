5f55f822cdd1ff6b290b5f64937982e3791d2235
Toby Ueno <tobyueno@gmail.com>
2022-09-09 16:32:49 -0700

    gpio mapping fix

--- rpi-driver/driver.cpp
+++ rpi-driver/driver.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-#include <led-matrix.h>
-
+#include "led-matrix.h"
+#include <unistd.h>
 
 using namespace rgb_matrix;
 
-int main() {
-	RGBMatrix::Options defaultOptions;
-	std::cout << "Hello!\n";
+static void draw(Canvas *canvas) {
+	canvas->Fill(0, 0, 255);
+	std::cout << "test";
+	sleep(5);
 }
+
+int main(int argc, char *argv[]) {
+	RGBMatrix::Options defaultOptions;
+	defaultOptions.rows = 64;
+	defaultOptions.cols = 64;
+	defaultOptions.hardware_mapping = "adafruit-hat";
+	defaultOptions.chain_length = 1;
+	defaultOptions.parallel = 1;
+	defaultOptions.show_refresh_rate = true;
+
+	Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaultOptions);
+	if (canvas == NULL) return 1;
+	canvas->Fill(0, 0, 255);
+	sleep(3);
+	canvas->Clear();
+	delete canvas;
+}
+
