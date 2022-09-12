1f8672170310f51f396911ddbc96b3f6d6938e3d
Toby Ueno <tobyueno@gmail.com>
2022-09-11 20:46:36 -0700

    misc

--- rpi_driver.py
+++ rpi_driver.py
@@ -1,15 +1,18 @@
 from matrix import Matrix
 from matrix_mapper_info import *
 from load_stops import load_stops
+from load_routes import load_routes
 from load_vehicles import load_vehicles
 import json
 import time
 
 
 if __name__ == "__main__":
-    matrix = Matrix()
-
+    
     load_stops()
+    load_routes()
+
+    matrix = Matrix()
 
     with open('data/stops.json') as f:
         stops = json.load(f)
