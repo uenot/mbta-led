fe23571ca6013c0773c8ace2083e10118c90bc78
uenot <tobyueno@gmail.com>
2022-09-11 23:51:50 -0400

    clear canvas fix

--- rpi_driver.py
+++ rpi_driver.py
@@ -14,22 +14,25 @@ if __name__ == "__main__":
     with open('data/stops.json') as f:
         stops = json.load(f)
 
-    while True:
-        vehicles = load_vehicles(void=False)
-        
-        for vehicle, vehicle_info in vehicles.items():
-            stop = vehicle_info['stop']
-            if stop is not None:
-                try:
-                    info = stop_coordinates[int(stop)]
-                except:
-                    continue
-                if vehicle_info['current_status'] in ['IN_TRANSIT_TO', 'INCOMING_AT']:
-                    flash = True
-                else:
-                    flash = False
-                matrix.set_pixel(info[0], info[1], colors[info[2]][0], colors[info[2]][1], colors[info[2]][2], flash=flash)
+    try:
+        while True:
+            vehicles = load_vehicles(void=False)
+            
+            for vehicle, vehicle_info in vehicles.items():
+                stop = vehicle_info['stop']
+                if stop is not None:
+                    try:
+                        info = stop_coordinates[int(stop)]
+                    except:
+                        continue
+                    if vehicle_info['current_status'] in ['IN_TRANSIT_TO', 'INCOMING_AT']:
+                        flash = True
+                    else:
+                        flash = False
+                    matrix.set_pixel(info[0], info[1], colors[info[2]][0], colors[info[2]][1], colors[info[2]][2], flash=flash)
 
-        matrix.run_cycle()
+            matrix.run_cycle()
+    except KeyboardInterrupt:
+        pass
 
 
