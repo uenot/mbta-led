35680dedbccae3026187cc2b91215fa57e11cd7d
uenot <tobyueno@gmail.com>
2022-10-31 17:01:28 -0400

    reconfig driver w/ new info format

--- rpi_driver.py
+++ rpi_driver.py
@@ -1,5 +1,5 @@
 from matrix import Matrix
-from matrix_mapper_info import *
+from matrix_mapper_info import stop_coordinates, colors
 from load_stops import load_stops
 from load_vehicles import load_vehicles
 import json
@@ -17,19 +17,25 @@ if __name__ == "__main__":
     try:
         while True:
             vehicles = load_vehicles(void=False)
+
+            set_pixels = set()
             
             for vehicle, vehicle_info in vehicles.items():
                 stop = vehicle_info['stop']
                 if stop is not None:
                     try:
-                        info = stop_coordinates[int(stop)]
+                        x, y = stop_coordinates[int(stop)]
                     except:
                         continue
                     if vehicle_info['current_status'] in ['IN_TRANSIT_TO', 'INCOMING_AT']:
                         flash = True
                     else:
                         flash = False
-                    matrix.set_pixel(info[0], info[1], colors[info[2]][0], colors[info[2]][1], colors[info[2]][2], flash=flash)
+                    if (x, y) in set_pixels:
+                        flash = not flash
+                    color = colors[stops[stop]['line']]
+                    matrix.set_pixel(x, y, color[0], color[1], color[2], flash=flash)
+                    set_pixels.add((x, y))
 
             matrix.run_cycle()
     except KeyboardInterrupt:
